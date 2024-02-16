b507763d3cc909a85ca6c52685e54e3d85abed1d
devarrowsmith <dave.arrowsmith1@justice.gov.uk>
2024-02-16 15:38:21 +0000

    Variables and Strings exercises.

---
+++ src/main/kotlin/VariablesAndStrings.kt
@@ -0,0 +1,21 @@
+// Strings & variables
+fun main(args: Array<String>) {
+
+    //var: Mutable, can be reassigned. Type can't change.
+    var name = "Dave"
+    println(name)
+    name = "David"
+    println(name)
+
+    // val: Immutable, can't be reassigned.
+    val catName = "My cat's name is \"Boris\"."
+    println(catName)
+
+    val bunnyLineOne = "(\\(\\"
+    val bunnyLineTwo = "(=.=)"
+    val bunnyLineThree = "o_(\")(\")"
+    println(bunnyLineOne)
+    println(bunnyLineTwo)
+    println(bunnyLineThree)
+}
+
