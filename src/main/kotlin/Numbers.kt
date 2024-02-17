7ea489907ea43105bd15f4c88b819c122366b46c
devarrowsmith <dave.arrowsmith1@justice.gov.uk>
2024-02-17 08:49:20 +0000

    Add notes on random number generation.

---
+++ src/main/kotlin/Numbers.kt
@@ -0,0 +1,15 @@
+import kotlin.random.Random
+
+
+fun main(args: Array<String>) {
+
+
+    // Random number between 0 and 10
+    println(Random.nextInt(10))
+
+    // Random number between 20 and 20 (can only return 20)
+    println(Random.nextInt(20, 21))
+
+    // Random number between 20 and 30
+    println(Random.nextInt(20, 31))
+}
