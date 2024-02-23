cae5bbb1b7e79b91e3d84d06df08d864d5e6cb7e
devarrowsmith <dave.arrowsmith1@justice.gov.uk>
2024-02-23 14:27:34 +0000

    Notes on collection types & lists.

---
+++ src/main/kotlin/collections/TypesOfCollections.kt
@@ -0,0 +1,27 @@
+package collections
+
+fun main(args: Array<String>) {
+    // Collections //////////
+
+    /*
+    - A way to group elements together.
+    - All values in a collection have the same type.
+    - Can have 0 or more elements.
+
+
+    Types of collection:
+    - Collection
+        - List
+            -MutableList
+        -Set
+            -MutableSet
+
+    // Maps /////////
+    Maps are NOT collections, but they're often grouped with collections as they have some similarities.
+     Types of map:
+     - Map
+        - MutableMap
+
+    */
+
+}
